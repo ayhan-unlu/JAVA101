f1e881a55c51d464a3f46824810637af1b0b611b
Ayhan Unlu <ayhan-unlu@hotmail.com>
2022-11-12 14:53:21 +0300

    Sorting Array Added

---
+++ 001_SingleClassProjects/221112_SortingArray/Practice_221112_SortingArray.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortingArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the number of elements You want in the array: ");
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("Please enter the number indexed \"" + i + "\" in the array: ");
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int tmp = 0;
+                if (arr[i] > arr[j]) {
+                    tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+        System.out.print("Sorted Array :");
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ 001_SingleClassProjects/221112_SortingArray/README_SortingArray.md
@@ -0,0 +1,5 @@
+Sorting Array
+
+Prepared for the Fullfillment of Java101 path in " ebebek Java & QA & SAP Spartacus Practicum " with Patika.Dev and Kodluyoruz.org in November 2022
+
+Write a Java program that sorts the elements of the array from smallest to largest. Get the size of the array and the elements of the array from the user.
