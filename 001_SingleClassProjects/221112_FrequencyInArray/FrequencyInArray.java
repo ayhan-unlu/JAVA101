3486bb11219715e0e30e21e85f6d9569086fba1f
Ayhan Unlu <ayhan-unlu@hotmail.com>
2022-11-12 15:04:57 +0300

    FrequencyInArray added

---
+++ 001_SingleClassProjects/221112_FrequencyInArray/FrequencyInArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class FrequencyInArray {
+    public static void main(String[] args) {
+        int[] array = new int[]{10, 20, 20, 10, 10, 20, 5, 20};
+        int[] frequencyArray = new int[]{1, 1, 1, 1, 1, 1, 1, 1};
+        for (int i = 0; i < array.length; i++) {
+            int frequency = 1;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+                    frequency++;
+                    frequencyArray[j] = 0;
+                }
+            }
+            if (frequencyArray[i] != 0) {
+                frequencyArray[i] = frequency;
+            }
+        }
+        System.out.println("Array : " + Arrays.toString(array));
+        System.out.println("Repeated Numbers in the Array :");
+        for (int i = 0; i < 8; i++) {
+            if (frequencyArray[i] != 0) {
+                System.out.println("Number " + array[i] + " is repeated " + frequencyArray[i] + " times");
+            }
+        }
+    }
+}
---
+++ 001_SingleClassProjects/221112_FrequencyInArray/README_FrequencyInArray.md
@@ -0,0 +1,5 @@
+Frequency In Array
+
+Prepared for the Fullfillment of Java101 path in " ebebek Java & QA & SAP Spartacus Practicum " with Patika.Dev and Kodluyoruz.org in November 2022
+
+Write a program in Java that finds the frequency of the elements in the array.
